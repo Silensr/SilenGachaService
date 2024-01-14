1c9f719e4edc863fbfb94eb4b51e43e2dcaa41d2
Tây Vo Tuan <votuantay@gmail.com>
2024-01-14 01:51:16 +0100

    Inital Commit + github oauth

---
+++ pages/login.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+const user = useUser();
+if (user.value) {
+	await navigateTo("/"); // redirect to profile page
+}
+</script>
+<template>
+	<h1>Sign in</h1>
+	<a href="/api/login/github">Sign in with GitHub</a>
+</template>
